b89c9ab14ebfdbee1848b0c0ed307aaa0727ec9b
Pumelela-Banca <pumelelabanca@gmail.com>
2023-06-23 02:11:28 +0200

    added test-3

--- push_pall.c
+++ push_pall.c
@@ -18,7 +18,7 @@ void push(stack_t **stack, unsigned int line_number)
 	if (look[1][0] == '\n' || !(look[1][0] > 47 && look[1][0] < 58)
 			|| look[1][0] != '-')
 	{
-		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: usage: push integer JJJJ\n", line_number);
 		free_rd(look);
 		exit(EXIT_FAILURE);
 	}
@@ -97,7 +97,7 @@ int find_int(unsigned int line_number)
 		else if (!(look[1][0] > 47 && look[1][0] < 58))
 		{
 			printf("jJJJJJ\n");
-			fprintf(stderr, "L%d: usage: push integer\n", line_number);
+			fprintf(stderr, "L%d: usage: push integer TEST\n", line_number);
 			free_rd(look);
 			exit(EXIT_FAILURE);
 		}
